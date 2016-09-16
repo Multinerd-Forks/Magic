d1541481469987890cf3e2bf96a19cb12ec850b5
wujingpeng <wujingpeng@youzan.com>
2016-09-16 12:27:37 +0800

    fix method name

--- Magic/Magic/Magic/Foundation/CalendarExtension.swift
+++ Magic/Magic/Magic/Foundation/CalendarExtension.swift
@@ -1,5 +1,5 @@
 //
-//  NSCalendar_extension.swift
+//  CalendarExtension.swift
 //  Magic
 //
 //  Created by Broccoli on 16/9/12.
@@ -9,38 +9,38 @@
 import Foundation
 
 extension Calendar {
-    public func dateByAddingSeconds(_ seconds: NSInteger, toDate date: Date) -> Date? {
+    public func date(byAddingSeconds seconds: Int, toDate date: Date) -> Date? {
         var components = DateComponents()
         components.second = seconds
-//        return self.date(byAdding: components, to: date, options: [])!
+
         return self.date(byAdding: components, to: date)
     }
     
-    public func dateByAddingMinutes(_ minutes: NSInteger, toDate date: Date) -> Date? {
+    public func date(byAddingMinutes minutes: Int, toDate date: Date) -> Date? {
         var components = DateComponents()
         components.minute = minutes
         return self.date(byAdding: components, to: date)
     }
     
-    public func dateByAddingHours(_ hours: NSInteger, toDate date: Date) -> Date? {
+    public func date(byAddingHours hours: Int, toDate date: Date) -> Date? {
         var components = DateComponents()
         components.hour = hours
         return self.date(byAdding: components, to: date)!
     }
     
-    public func dateByAddingDays(_ days: Int, toDate date: Date) -> Date? {
+    public func date(byAddingDays days: Int, toDate date: Date) -> Date? {
         var components = DateComponents()
         components.day = days
         return self.date(byAdding: components, to: date)
     }
     
-    public func dateByAddingMonths(_ months: Int, toDate date: Date) -> Date? {
+    public func date(byAddingMonths months: Int, toDate date: Date) -> Date? {
         var components = DateComponents()
         components.month = months
         return self.date(byAdding: components, to: date)
     }
     
-    public func dateByAddingYears(_ years: Int, toDate date: Date) -> Date? {
+    public func date(byAddingYears years: Int, toDate date: Date) -> Date? {
         var components = DateComponents()
         components.year = years
         return self.date(byAdding: components, to: date)
