924e6c21801407ab34996923f53dc75dd5e17cb1
wujingpeng <wujingpeng@youzan.com>
2016-09-17 00:09:16 +0800

    add UserDefaultsExtension

---
+++ Magic/Magic/Magic/Foundation/UserDefaultsExtension.swift
@@ -0,0 +1,21 @@
+//
+//  UserDefaultsExtension.swift
+//  Magic
+//
+//  Created by Broccoli on 16/9/16.
+//  Copyright © 2016年 broccoliii. All rights reserved.
+//
+
+import Foundation
+
+extension UserDefaults {
+    subscript(key: String) -> Any? {
+        get {
+            return object(forKey: key)
+        }
+        
+        set {
+            set(newValue, forKey: key)
+        }
+    }
+}
