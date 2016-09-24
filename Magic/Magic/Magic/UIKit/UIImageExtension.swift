7af2c509df5f11c835aeeb82c156232766a97202
wujingpeng <wujingpeng@youzan.com>
2016-09-24 12:32:13 +0800

    add some method

--- Magic/Magic/Magic/UIKit/UIImageExtension.swift
+++ Magic/Magic/Magic/UIKit/UIImageExtension.swift
@@ -40,6 +40,25 @@ public extension UIImage {
         let croppedImageRef = cgImage!.cropping(to: rect)
         return UIImage(cgImage: croppedImageRef!)
     }
+    
+    public func tint(_ tintColor: UIColor) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        
+        let context = UIGraphicsGetCurrentContext()
+        context?.translateBy(x: 0, y: self.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0);
+        context?.setBlendMode(CGBlendMode.normal)
+        
+        let rect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height) as CGRect
+        context?.clip(to: rect, mask: self.cgImage!)
+        tintColor.setFill()
+        context?.fill(rect)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()! as UIImage
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
 }
 
 public extension UIImage {
