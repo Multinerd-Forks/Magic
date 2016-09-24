a17ee0c3396887598151d0275b87c46ab348bb95
wujingpeng <wujingpeng@youzan.com>
2016-09-24 10:15:26 +0800

    add UIViewExtension

--- Magic/Magic/Magic/UIKit/UIViewControllerExtension.swift
+++ Magic/Magic/Magic/UIKit/UIViewControllerExtension.swift
@@ -7,9 +7,78 @@
 //
 
 import Foundation
+import SafariServices
 
 public extension UIViewController {
     func dismissKeyboard() {
         view.endEditing(true)
     }
+}
+
+public extension UIViewController {
+    
+//    public func presentAlert(title: String,
+//                             message: String? = nil,
+//                             buttonText: String = "OK",
+//                             additionalActions: [UIAlertAction]? = nil,
+//                             preferredStyle: UIAlertControllerStyle = .alert,
+//                             includeCancelAction: Bool = false,
+//                             handler: (() -> Void)? = nil) {
+//        let alertController = UIAlertController(
+//            title: title,
+//            message: message,
+//            preferredStyle: preferredStyle
+//        )
+//        
+//        if includeCancelAction {
+//            alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+//        }
+//        
+//        if let additionalActions = additionalActions {
+//            additionalActions.forEach { item in
+//                alertController.addAction(item)
+//            }
+//        }
+//        
+//        alertController.addAction(UIAlertAction(title: buttonText) { handler?() })
+//        
+//        present(alertController, animated: true, completion: nil)
+//    }
+    
+    @available(iOSApplicationExtension 9.0, *)
+    public func presentSafariController(url: String, modalPresentationStyle: UIModalPresentationStyle = .overFullScreen) {
+        let safariController = SFSafariViewController(url: NSURL(string: url)! as URL)
+        safariController.modalPresentationStyle = .overFullScreen
+        
+        safariController.delegate = self as? SFSafariViewControllerDelegate
+        
+        present(safariController, animated: true, completion: nil)
+    }
+    
+
+    public func presentActivityViewController(activityItems: [AnyObject],
+                                              sourceView: UIView, applicationActivities: [UIActivity]? = nil) {
+        let activity = UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+        
+        if let popOver = activity.popoverPresentationController {
+            popOver.sourceView = sourceView
+            popOver.sourceRect = sourceView.bounds
+            popOver.permittedArrowDirections = .any
+        }
+        
+        present(activity, animated: true, completion: nil)
+    }
+    
+    public func presentActivityViewController(activityItems: [AnyObject],
+                                              barButtonItem: UIBarButtonItem, applicationActivities: [UIActivity]? = nil) {
+        let activity = UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+        
+        if let popOver = activity.popoverPresentationController {
+            popOver.barButtonItem  = barButtonItem
+            popOver.permittedArrowDirections = .any
+        }
+        
+        present(activity, animated: true, completion: nil)
+    }
+    
 }
