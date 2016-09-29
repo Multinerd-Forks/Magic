87cba3f9a5492bb6750f28a77cd6f7c92d83e652
wujingpeng <wujingpeng@youzan.com>
2016-09-30 00:23:41 +0800

    add code for model

--- MagicExample/MagicExample/MasterTableViewController.swift
+++ MagicExample/MagicExample/MasterTableViewController.swift
@@ -11,15 +11,18 @@ import Magic
 
 class MasterTableViewController: UITableViewController {
     let demoArray = ["TableViewDemo"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
 
-    // MARK: - Table view data source
+// MARK: - Table view data source
+extension MasterTableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "MasterTableViewCell")
         if indexPath.row == 0 {
@@ -27,17 +30,12 @@ class MasterTableViewController: UITableViewController {
         }
         return cell
     }
-   
+    
     // MARK: - Table view delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
             let tableViewController = storyboard?.instantiateViewController(viewController:  TableViewController.self)
             navigationController?.pushViewController(tableViewController!, animated: true)
         }
-    }
-
-    // MARK: - Navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
     }
 }
