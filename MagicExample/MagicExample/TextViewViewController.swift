051994da4290cdb11e5270b62be2131bdb7b39e0
wujingpeng <wujingpeng@youzan.com>
2016-10-01 08:05:51 +0800

    add some file

---
+++ MagicExample/MagicExample/TextViewViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TextViewViewController.swift
+//  MagicExample
+//
+//  Created by Broccoli on 2016/9/30.
+//  Copyright © 2016年 broccoliii. All rights reserved.
+//
+
+import UIKit
+
+class TextViewViewController: UIViewController {
+
+    @IBOutlet var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textView.placeholder = "placeholder"
+        print(textView.placeholder)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textView.placeholder = "placeholderplaceholderplaceholderplaceholderplaceholderplaceholderplaceholderplaceholderplaceholder"
+    }
+}
