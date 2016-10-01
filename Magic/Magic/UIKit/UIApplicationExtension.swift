5b24ea9382ee604cbbb6e42a7f5be9fb741a05b6
wujingpeng <wujingpeng@youzan.com>
2016-10-01 08:12:04 +0800

    fix UIApplicationExtension

--- Magic/Magic/UIKit/UIApplicationExtension.swift
+++ Magic/Magic/UIKit/UIApplicationExtension.swift
@@ -9,27 +9,28 @@
 import Foundation
 
 public extension UIApplication {
-    func registerUserNotificationSettings(
-        identifier: String,
-        actions: [UIMutableUserNotificationAction]? = nil,
-        type: UIUserNotificationType = [ .alert, .badge, .sound ]) {
-        
-        var mainCategory: UIMutableUserNotificationCategory? = nil
-        
-        if let a = actions , a.count > 0 {
-            mainCategory = UIMutableUserNotificationCategory()
-            mainCategory!.identifier = identifier
-            mainCategory!.setActions(a, for: .default)
-            mainCategory!.setActions(a, for: .minimal)
-        }
-        
-        let notificationSettings = UIUserNotificationSettings(
-            types: type,
-            categories: mainCategory != nil
-                ? [mainCategory!]
-                : nil)
-        registerUserNotificationSettings(notificationSettings)
-    }
+    
+//    func registerUserNotificationSettings(
+//        identifier: String,
+//        actions: [UIMutableUserNotificationAction]? = nil,
+//        type: UIUserNotificationType = [ .alert, .badge, .sound ]) {
+//        
+//        var mainCategory: UIMutableUserNotificationCategory? = nil
+//        
+//        if let a = actions , a.count > 0 {
+//            mainCategory = UIMutableUserNotificationCategory()
+//            mainCategory!.identifier = identifier
+//            mainCategory!.setActions(a, for: .default)
+//            mainCategory!.setActions(a, for: .minimal)
+//        }
+//        
+//        let notificationSettings = UIUserNotificationSettings(
+//            types: type,
+//            categories: mainCategory != nil
+//                ? [mainCategory!]
+//                : nil)
+//        registerUserNotificationSettings(notificationSettings)
+//    }
 
     func removeLocalNotification(identifier: String) {
         guard let notifications = scheduledLocalNotifications
@@ -49,13 +50,13 @@ public extension UIApplication {
         applicationIconBadgeNumber = 0
     }
     
-    @available(iOSApplicationExtension 9.0, *)
-    func updateShortcutItem(type: String, handler: (UIMutableApplicationShortcutItem) -> UIMutableApplicationShortcutItem) {
-        guard let index = shortcutItems?.index(where: { $0.type == type }),
-            let item = shortcutItems?[index].mutableCopy() as? UIMutableApplicationShortcutItem else {
-                return
-        }
-        
-        shortcutItems?[index] = handler(item)
-    }
+//    @available(iOSApplicationExtension 9.0, *)
+//    func updateShortcutItem(type: String, handler: (UIMutableApplicationShortcutItem) -> UIMutableApplicationShortcutItem) {
+//        guard let index = shortcutItems?.index(where: { $0.type == type }),
+//            let item = shortcutItems?[index].mutableCopy() as? UIMutableApplicationShortcutItem else {
+//                return
+//        }
+//        
+//        shortcutItems?[index] = handler(item)
+//    }
 }
