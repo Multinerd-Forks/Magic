7bf1b902a1361c687c796a321919789aead90635
wujingpeng <wujingpeng@youzan.com>
2016-10-11 20:18:07 +0800

    Add BoolExtension

---
+++ Magic/Magic/Foundation/BoolExtension.swift
@@ -0,0 +1,15 @@
+//
+//  BoolExtension.swift
+//  Magic
+//
+//  Created by Broccoli on 2016/10/11.
+//  Copyright © 2016年 broccoliii. All rights reserved.
+//
+
+import Foundation
+
+extension Bool {
+     public func toInt() -> Int {
+        return self ? 1 : 0
+    }
+}
