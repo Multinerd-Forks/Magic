65e2b6bd388c1242f3a717a17b0e19fef1df2762
wujingpeng <wujingpeng@youzan.com>
2016-10-11 20:18:43 +0800

    Add TimerExtension

---
+++ Magic/Magic/Foundation/TimerExtension.swift
@@ -0,0 +1,23 @@
+//
+//  TimerExtension.swift
+//  Magic
+//
+//  Created by Broccoli on 2016/10/11.
+//  Copyright © 2016年 broccoliii. All rights reserved.
+//
+
+import Foundation
+
+extension Timer {
+    public static func every(seconds: TimeInterval, handler: @escaping (CFRunLoopTimer?) -> Void) -> Timer {
+        let fireDate = CFAbsoluteTimeGetCurrent()
+        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, seconds, 0, 0, handler)
+        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, .commonModes)
+        return timer!
+    }
+    
+    public static func delay(seconds: Double, queue: DispatchQueue = .global(qos: .background), after: @escaping ()->()) {
+        let time = DispatchTime.now() + Double(Int64(seconds * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+        queue.asyncAfter(deadline: time, execute: after)
+    }
+}
