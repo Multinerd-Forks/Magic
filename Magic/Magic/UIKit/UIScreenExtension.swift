c51d97f6189232f765597274e11fe7d525fe2fef
wujingpeng <wujingpeng@youzan.com>
2016-10-12 09:59:21 +0800

    Add UIScreenExtension

---
+++ Magic/Magic/UIKit/UIScreenExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UIScreenExtension.swift
+//  Magic
+//
+//  Created by Broccoli on 2016/10/11.
+//  Copyright © 2016年 broccoliii. All rights reserved.
+//
+
+import Foundation
+
+extension UIScreen {
+//    static var scale: Double {
+//        return UIScreen.main.scale
+//    }
+}
