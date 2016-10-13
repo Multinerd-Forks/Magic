7f81708fc6bcd1874a30242a541d05966280ce76
wujingpeng <wujingpeng@youzan.com>
2016-10-13 16:27:31 +0800

    Add UIGestureRecognizerExtension

---
+++ Magic/Magic/UIKit/UIGestureRecognizerExtension.swift
@@ -0,0 +1,29 @@
+//
+//  UIGestureRecognizerExtension.swift
+//  Magic
+//
+//  Created by Broccoli on 2016/10/12.
+//  Copyright © 2016年 broccoliii. All rights reserved.
+//
+
+import Foundation
+
+extension UIGestureRecognizer {
+    fileprivate class ActionStorage {
+        
+        var control: UIGestureRecognizer!
+        var action: ((UIGestureRecognizer) -> Void)!
+        
+        @objc func triggerEventAction() {
+            action(control)
+        }
+    }
+    
+    convenience init(action: @escaping (UIGestureRecognizer) -> ()) {
+        let actionStorage = ActionStorage()
+        self.init(target: actionStorage, action: #selector(ActionStorage.triggerEventAction))
+        actionStorage.control = self
+        actionStorage.action = action
+    }
+}
+
