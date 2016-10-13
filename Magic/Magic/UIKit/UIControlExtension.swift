d180fb85eb3e2d174cdf4c7f87f34a96c7b67779
wujingpeng <wujingpeng@youzan.com>
2016-10-13 16:27:13 +0800

    Add UIControlExtension

---
+++ Magic/Magic/UIKit/UIControlExtension.swift
@@ -0,0 +1,34 @@
+//
+//  UIControlExtension.swift
+//  Magic
+//
+//  Created by Broccoli on 2016/10/12.
+//  Copyright © 2016年 broccoliii. All rights reserved.
+//
+
+import Foundation
+
+extension UIControl {
+
+    fileprivate class ActionStorage {
+        
+        var control: UIControl
+        var action: (UIControl) -> Void
+        
+        init(_ control: UIControl, action: @escaping (UIControl) -> Void) {
+            self.control = control
+            self.action = action
+        }
+        
+        @objc func triggerEventAction() {
+            action(control)
+        }
+    }
+    
+    func actionHandle(for controlEvent: UIControlEvents, action: @escaping (UIControl) -> Void) {
+        let actionStorage = ActionStorage(self, action: action)
+       addTarget(actionStorage, action: #selector(ActionStorage.triggerEventAction), for: controlEvent)
+    }
+}
+
+
