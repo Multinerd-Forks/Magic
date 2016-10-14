78272ce2830c78e9825c0c323b5493c6b6bcb505
wujingpeng <wujingpeng@youzan.com>
2016-10-14 10:28:02 +0800

    Add UIBarButtonItemExtension

---
+++ Magic/Magic/UIKit/UIBarButtonItemExtension.swift
@@ -0,0 +1,13 @@
+//
+//  UIBarButtonItemExtension.swift
+//  Magic
+//
+//  Created by Broccoli on 2016/10/13.
+//  Copyright © 2016年 broccoliii. All rights reserved.
+//
+
+import Foundation
+
+extension UIBarButtonItem {
+   
+}
