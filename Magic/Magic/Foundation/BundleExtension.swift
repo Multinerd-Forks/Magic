c78cc49edf93f03f10a28d60d9f946900af3ff37
wujingpeng <wujingpeng@youzan.com>
2016-10-14 14:41:36 +0800

    Add BundleExtension.swift

---
+++ Magic/Magic/Foundation/BundleExtension.swift
@@ -0,0 +1,53 @@
+//
+//  BundleExtension.swift
+//  Magic
+//
+//  Created by Broccoli on 2016/10/14.
+//  Copyright © 2016年 broccoliii. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    static var name: String? {
+        if let bundleDisplayName = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String {
+            return bundleDisplayName
+        } else if let bundleName = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String {
+            return bundleName
+        }
+        
+        return nil
+    }
+    
+    static var appVersion: String {
+        return Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
+    }
+    
+    static var build: String {
+        return Bundle.main.object(forInfoDictionaryKey: String(kCFBundleVersionKey)) as! String
+    }
+    
+    static var bundleIdentifier: String {
+        return Bundle.main.infoDictionary!["CFBundleIdentifier"] as! String
+    }
+    
+    static var bundleName: String {
+        return Bundle.main.infoDictionary!["CFBundleName"] as! String
+    }
+    
+    /**
+     App's icon image
+     
+     - returns: UIImage
+     */
+    public static func icon() -> UIImage? {
+        let iconFilename = Bundle.main.object(forInfoDictionaryKey: "CFBundleIconFile")
+        let iconBasename = (iconFilename as! NSString).deletingPathExtension
+        let iconExtension = (iconFilename as! NSString).pathExtension
+        let iconFile = Bundle.main.path(forResource: iconBasename, ofType: iconExtension)!
+        guard let image = UIImage(contentsOfFile:iconFile) else {
+            return nil
+        }
+        return image
+    }
+}
