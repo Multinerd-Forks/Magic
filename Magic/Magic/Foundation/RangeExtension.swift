ab86237de97767c1d7da416d8ed3945925a3caf4
wujingpeng <wujingpeng@youzan.com>
2016-10-14 10:27:49 +0800

    Add RangeExtension

---
+++ Magic/Magic/Foundation/RangeExtension.swift
@@ -0,0 +1,28 @@
+//
+//  RangeExtension.swift
+//  Magic
+//
+//  Created by Broccoli on 2016/10/13.
+//  Copyright © 2016年 broccoliii. All rights reserved.
+//
+
+import Foundation
+
+public extension CountableClosedRange {
+    func each(callback: (Element) -> (Void)) {
+        for index in self {
+            callback(index)
+        }
+    }
+    
+    func each(callback: @escaping (Void) -> (Void)) {
+        for _ in self {
+            callback()
+        }
+    }
+    
+}
+
+func ==<T: Comparable>(left: Range<T>, right: Range<T>) -> Bool {
+    return left.lowerBound == right.lowerBound && left.upperBound == right.upperBound
+}
