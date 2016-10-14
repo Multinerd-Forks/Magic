3df2e927fccc344147bbd6089fbe71f9c97310f7
wujingpeng <wujingpeng@youzan.com>
2016-10-14 16:08:14 +0800

    Add UINavigationBarExtension

---
+++ Magic/Magic/UIKit/UINavigationBarExtension.swift
@@ -0,0 +1,13 @@
+//
+//  UINavigationBarExtension.swift
+//  Magic
+//
+//  Created by Broccoli on 2016/10/14.
+//  Copyright © 2016年 broccoliii. All rights reserved.
+//
+
+import Foundation
+
+extension UINavigationBar {
+    
+}
