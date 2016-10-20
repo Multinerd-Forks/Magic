982be89eaf7a2865f5cd908ee6f392ff208e8eea
wujingpeng <wujingpeng@youzan.com>
2016-10-20 13:27:44 +0800

    Update UINavigationBarExtension

--- Magic/Magic/UIKit/UINavigationBarExtension.swift
+++ Magic/Magic/UIKit/UINavigationBarExtension.swift
@@ -10,4 +10,10 @@ import Foundation
 
 extension UINavigationBar {
     
+    func setBottomBorderColor(color: UIColor, height: CGFloat) {
+        let bottomBorderRect = CGRect(x: 0, y: frame.height, width: frame.width, height: height)
+        let bottomBorderView = UIView(frame: bottomBorderRect)
+        bottomBorderView.backgroundColor = color
+        addSubview(bottomBorderView)
+    }
 }
