9c3a2795106feb13b9e55d3600fb4f64bffb7fdd
wujingpeng <wujingpeng@youzan.com>
2016-10-21 10:01:53 +0800

    Update UIButtonExtension

--- Magic/Magic/UIKit/UIButtonExtension.swift
+++ Magic/Magic/UIKit/UIButtonExtension.swift
@@ -10,7 +10,7 @@ import Foundation
 
 // function
 public extension UIButton {
-    func setBackgroundColor(_ color: UIColor, forState: UIControlState) {
+   func setBackgroundColor(_ color: UIColor, forState: UIControlState) {
         UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
         UIGraphicsGetCurrentContext()?.setFillColor(color.cgColor)
         UIGraphicsGetCurrentContext()?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
