c804b11f9025d6d93cd9bfd3d09ebfb83bf98083
wujingpeng <wujingpeng@youzan.com>
2016-10-21 10:02:01 +0800

    Update App.swift

--- Magic/Magic/Struct/App.swift
+++ Magic/Magic/Struct/App.swift
@@ -9,39 +9,5 @@
 import Foundation
 
 public struct App {
-    public static var name: String? {
-        return Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String
-    }
-    
-    public static var version: String {
-        return Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
-    }
-    
-    public static var build: String {
-        return Bundle.main.object(forInfoDictionaryKey: String(kCFBundleVersionKey)) as! String
-    }
-    
-    public static var bundleIdentifier: String {
-        return Bundle.main.infoDictionary!["CFBundleIdentifier"] as! String
-    }
-    
-    public static var bundleName: String {
-        return Bundle.main.infoDictionary!["CFBundleName"] as! String
-    }
-    
-    public static var screenOrientation: UIInterfaceOrientation {
-        return UIApplication.shared.statusBarOrientation
-    }
-    
-    public static var screenStatusBarHeight: CGFloat {
-        return UIApplication.shared.statusBarFrame.height
-    }
-    
-    public static var screenHeightWithoutStatusBar: CGFloat {
-        if UIInterfaceOrientationIsPortrait(screenOrientation) {
-            return UIScreen.main.bounds.size.height - screenStatusBarHeight
-        } else {
-            return UIScreen.main.bounds.size.width - screenStatusBarHeight
-        }
-    }
+   
 }
