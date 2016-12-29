37ca77683c81135f300129643fb1e5b5166c7832
wujingpeng <wujingpeng@youzan.com>
2016-12-29 17:30:02 +0800

    Add DispatchTime

---
+++ Magic/Magic/DispatchTimeExtension.swift
@@ -0,0 +1,21 @@
+//
+//  DispatchTimeExtension.swift
+//  Magic
+//
+//  Created by Broccoli on 2016/12/29.
+//  Copyright © 2016年 broccoliii. All rights reserved.
+//
+
+import Foundation
+
+extension DispatchTime: ExpressibleByIntegerLiteral {
+    public init(integerLiteral value: Int) {
+        self = DispatchTime.now() + .seconds(value)
+    }
+}
+
+extension DispatchTime: ExpressibleByFloatLiteral {
+    public init(floatLiteral value: Double) {
+        self = DispatchTime.now() + .milliseconds(Int(value * 1000))
+    }
+}
