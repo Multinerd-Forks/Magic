a49669fe820ecd0d8909de81e082e20c77a90954
wujingpeng <wujingpeng@youzan.com>
2017-01-04 18:44:22 +0800

    Add comments

--- Magic/Magic/DispatchTimeExtension.swift
+++ Magic/Magic/DispatchTimeExtension.swift
@@ -8,12 +8,14 @@
 
 import Foundation
 
+// MARK: - ExpressibleByIntegerLiteral
 extension DispatchTime: ExpressibleByIntegerLiteral {
     public init(integerLiteral value: Int) {
         self = DispatchTime.now() + .seconds(value)
     }
 }
 
+// MARK: - ExpressibleByFloatLiteral
 extension DispatchTime: ExpressibleByFloatLiteral {
     public init(floatLiteral value: Double) {
         self = DispatchTime.now() + .milliseconds(Int(value * 1000))
