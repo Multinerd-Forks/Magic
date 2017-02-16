7598bdc7d6e4640758e2cfc308dac78ce587853d
wujingpeng <wujingpeng@youzan.com>
2017-02-16 10:18:12 +0800

    SetAssociatedObject support generic

--- Magic/Magic/Foundation/NSObjectExtension.swift
+++ Magic/Magic/Foundation/NSObjectExtension.swift
@@ -21,12 +21,12 @@ public extension NSObject {
 
 // MARK: - Methods
 public extension NSObject {
-    func setAssociatedObject(_ value: AnyObject?, associativeKey: UnsafeRawPointer, policy: objc_AssociationPolicy) {
+    func setAssociatedObject<T>(_ value: T?, associativeKey: UnsafeRawPointer, policy: objc_AssociationPolicy) {
         if let valueAsAnyObject = value {
             objc_setAssociatedObject(self, associativeKey, valueAsAnyObject, policy)
         }
     }
-    
+
     func getAssociatedObject(_ associativeKey: UnsafeRawPointer) -> Any? {
         guard let valueAsType = objc_getAssociatedObject(self, associativeKey) else {
             return nil
